cb3a05af28f3a457331653ad61d40e75e5831ac5
ImKyd <kralikmatej@seznam.cz>
2017-05-15 20:59:45 +0200

    Update Game.php

--- src/Annihilation/Game/Game.php
+++ src/Annihilation/Game/Game.php
@@ -1,1 +1,14 @@
+<?php
 
+namespace Annihilation\Game;
+
+class Game{
+  public function __construct($id, Annihilation $plugin){
+    $this->id = $id;
+    $this->plugin = $plugin;
+    $this->redspawn = $plugin->data[$this->id]["spawn1"];
+    $this->bluespawn = $plugin->data[$this->id]["spawn2"];
+    $this->greenspawn = $plugin->data[$this->id]["spawn3"];
+    $this->yellspawn = $plugin->data[$this->id]["spawn4"];
+  }
+}
