ff000ad74a2691aff0c5b2d5d0f59ba11bc33340
ImKyd <kralikmatej@seznam.cz>
2017-05-16 18:37:03 +0200

    Update Player.php

--- src/Annihilation/Object/Player.php
+++ src/Annihilation/Object/Player.php
@@ -1,1 +1,30 @@
+<?php
 
+namespace Annihilation\Object;
+
+class Player{
+  public $team = null;
+  public $name;
+  public $wasingame = false;
+  public $kit = "civilian";
+  
+  public function __construct($name){
+    $this->name = $name;
+  }
+  
+  public function getTeam(){
+    return $this->team;
+  }
+  
+  public function setTeam($team){
+    $this->team = $team;
+  }
+  
+  public function getKit(){
+    return $this->kit;
+  }
+  
+  public function setKit($kit){
+    $this->kit = $kit;
+  }
+}
